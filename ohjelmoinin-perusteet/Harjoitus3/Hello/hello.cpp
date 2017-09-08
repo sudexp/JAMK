dd20764b11e81432cd166c89efa5984a0a042650
Dmitry Sklyarov <sudexp@icloud.com>
2017-09-08 20:18:16 +0300

    Added exercise 3,4,5 cpp

---
+++ ohjelmoinin-perusteet/Harjoitus3/Hello/hello.cpp
@@ -0,0 +1,13 @@
+/*
+Kommentti alkaa t‰st‰ ja jatku...
+*/
+#include <iostream>
+using namespace std;
+int main() // p‰‰ohjelma alkaa t‰st‰
+{ // aaltosulku aloittaa p‰‰ohjelman
+	cout << "Hei maailma!\n";
+	cout << "Hyvin menee"
+		 << endl
+		 << "mutta menkˆˆn" 
+		 << endl;
+} // aaltosulku lopettaa p‰‰ohjelman
---
+++ ohjelmoinin-perusteet/Harjoitus5/harj5.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int luku1;
+	int luku2;
+	cout << "Syötä luku1: ";
+	cin >> luku1;
+	cout << "Syötä luku2: ";
+	cin >> luku2;
+
+	if (luku1 == luku2)
+		cout << "BINGO " << endl;
+	else
+		cout << "BONGO " << endl;
+	return 0;
+}
