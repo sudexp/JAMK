557d7c2076a48dc6c18c5d1b8ab0fe98cad121ae
sudexp <sudexp@icloud.com>
2018-03-24 21:45:02 +0200

    switching by buttons left-right done

--- game/intro.js
+++ game/intro.js
@@ -17,13 +17,17 @@ window.onload = function() {
     };
 };
 
-function next (number) {
-    // document.getElementById('instructions').style.display = 'none'
+function back(number) {
+    $('#instruction-' + (number+1)).hide();
+    $('#instruction-' + number).show();
+}
+
+function next(number) {
     $('#instruction-' + (number-1)).hide();
     $('#instruction-' + number).show();
 }
+
 function start() {
-    // document.getElementById('instructions').style.display = 'none'
     $('#background_intro').hide();
     $('#intro').hide();
     init();
