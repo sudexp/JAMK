f8b518ed98e9146e27e7332fea0f804933910c3c
sudexp <sudexp@icloud.com>
2018-03-30 16:29:50 +0300

    intro.js: clean up

--- game/intro.js
+++ game/intro.js
@@ -1,32 +1,29 @@
-// var video = document.getElementById("animation");
-// aud.addEventListener("ended", function() {
-//     alert("The video has ended");
-// });
-
+// script execution after page load
 window.onload = function() {
     var video = document.getElementById('animation');
     var skip = document.getElementById('skip_button');
     var wrap = document.getElementById('wrap');
-
+    // ability to cancel viewing video and go to function "video.onended"
     skip.addEventListener('click', function () {
         video.onended();
     }, false);
+    // go to the game menu
     video.onended = function() {
         wrap.remove();
         document.getElementById('background_intro').style.display = 'block';
     };
 };
-
+// scrolling menu to the left
 function back(number) {
     $('#instruction-' + (number+1)).hide();
     $('#instruction-' + number).show();
 }
-
+// scrolling menu to the right
 function next(number) {
     $('#instruction-' + (number-1)).hide();
     $('#instruction-' + number).show();
 }
-
+// starting function "init" in "game.js" - start of the game
 function start() {
     $('#background_intro').hide();
     $('#intro').hide();
