8e8a31457bbaf0780183857fd436c0f0e80110fd
sudexp <sudexp@icloud.com>
2018-09-13 00:22:49 +0300

    Exercise 4 modified

--- web-ohjelmointi/harj2/tehtava4/script.js
+++ web-ohjelmointi/harj2/tehtava4/script.js
@@ -7,31 +7,46 @@ class Calculator {
   }
   
   get speedKmH() {
-    return this.calcKmhPace();
+    return this.calcSpeedKmH();
   }
 
-  calcKmhPace() {
-    return this.distance / (this.hours + this.minutes / 60 + this.seconds / 3600);
+  calcSpeedKmH() {
+    return (
+      Math.round(
+        (this.distance /
+          (this.hours + this.minutes / 60 + this.seconds / 3600)) *
+          100
+      ) / 100
+    );
   }
 
   get speedMinKm() {
-    return this.calcMinkmPace();
+    return this.calcSpeedMinKm();
   }
 
-  calcMinkmPace() {
-    return (this.hours * 60 + this.minutes + this.seconds / 60) / this.distance;
+  calcSpeedMinKm() {
+    return (
+      Math.round(
+        ((this.hours * 60 + this.minutes + this.seconds / 60) / this.distance) *
+          100
+      ) / 100
+    );
   }
 }
 
+//let hours = parseInt(document.getElementById('hours').value);
+//let minutes = parseInt(document.getElementById('minutes').value);
+//let seconds = parseInt(document.getElementById('seconds').value);
+//let distance = parseInt(document.getElementById('seconds').value);
+
 const speed = new Calculator(1, 13, 13, 20);
+//const speed = new Calculator(hours, minutes, seconds, distance);
 
 console.log(speed.speedKmH, speed.speedMinKm);
 
-// function addition() {
-//   var a = parseFloat(document.getElementById('number1').value);
-//   var b = parseFloat(document.getElementById('number2').value);
-//   // if (isNaN(a) === true) a = 0;
-//   // if (isNaN(b) === true) b = 0;
-//   var c = a + b;
-//   document.getElementById('result').innerHTML = ' Summa = ' + c;
-// }
+function outputOnDisplay() {
+  document.getElementById('speed-km-h').innerHTML = speed.speedKmH + ' km/h';
+  document.getElementById('speed-km-h').style.display = 'block';
+  document.getElementById('speed-min-km').innerHTML = speed.speedMinKm + ' min/km';
+  document.getElementById('speed-min-km').style.display = 'block';
+}
