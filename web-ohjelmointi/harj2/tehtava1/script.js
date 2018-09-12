c7ff1e4d7ce4eac11f42dccc925fac94254aff42
sudexp <sudexp@icloud.com>
2018-09-12 20:30:30 +0300

    Exercises 1,2 modified

--- web-ohjelmointi/harj2/tehtava1/script.js
+++ web-ohjelmointi/harj2/tehtava1/script.js
@@ -1,6 +1,6 @@
 function addition() {
-  var a = parseInt(document.getElementById('number1').value);
-  var b = parseInt(document.getElementById('number2').value);
+  var a = parseFloat(document.getElementById('number1').value);
+  var b = parseFloat(document.getElementById('number2').value);
   // if (isNaN(a) === true) a = 0;
   // if (isNaN(b) === true) b = 0;
   var c = a + b;
--- web-ohjelmointi/harj2/tehtava2/script.js
+++ web-ohjelmointi/harj2/tehtava2/script.js
@@ -1,8 +1,6 @@
-window.onload = writeResults;
-
 const arr = [11, 22, 33, 44];
 
-let printArray = function() {
+let printArray = function(arr) {
   let i;
   let arrContent = '';
   for (i in arr) {
@@ -11,11 +9,12 @@ let printArray = function() {
   return arrContent;
 };
 
-let arrayCount = function() {
-  return arr.length;
+let arrayCount = function(arr) {
+  const len = arr.length;
+  return len;
 };
 
-let arraySum = function() {
+let arraySum = function(arr) {
   let sum = 0;
   let i;
   for (i of arr) {
@@ -24,16 +23,15 @@ let arraySum = function() {
   return sum;
 };
 
-let arrayAvg = function() {
-  let avg;
-  avg = Math.round((sum / arr.length) * 10) / 10;
+let arrayAvg = function(len, sum) {
+  const avg = Math.round((sum / len) * 10) / 10;
   return avg;
 };
 
-let print = printArray();
-let count = arrayCount();
-let sum = arraySum();
-let avg = arrayAvg();
+const print = printArray(arr);
+const count = arrayCount(arr);
+const summa = arraySum(arr);
+const avg = arrayAvg(count, summa);
 
 function writeResults() {
   document.getElementById('result').innerHTML =
@@ -43,8 +41,10 @@ function writeResults() {
     count +
     '<br>' +
     'Summa on ' +
-    sum +
+    summa +
     '<br>' +
     'Keskiarvo on ' +
     avg;
 }
+
+window.onload = writeResults;
