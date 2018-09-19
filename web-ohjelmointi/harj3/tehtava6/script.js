aeac5324fba1dc026e9b08666893a1910f64d059
sudexp <sudexp@icloud.com>
2018-09-19 21:49:58 +0300

    Exercise 5 modified

--- web-ohjelmointi/harj3/tehtava6/script.js
+++ web-ohjelmointi/harj3/tehtava6/script.js
@@ -1,29 +1,25 @@
 function selectColor(value) {
-  let color = document.getElementById('color');
+  const color = document.getElementById('color');
   color.innerHTML = 'Valittu väri: ' + value;
 }
 
 function changeColor() {
-  let printArea = document.getElementById('printarea');
-  let colorStyle = printArea.style.backgroundColor;
-  colorStyle = document.getElementById('selectcolor').value;
+  const printArea = document.getElementById('print-area');
+  colorStyle = document.getElementById('select-color').value;
+  printArea.style.backgroundColor = colourNameToHex(colorStyle);
 }
 
-// function colourNameToHex(colour) {
-//   var colours = {
-//     beige: '#f5f5dc',
-//     blue: '#0000ff',
-//     brown: '#a52a2a',
-//     gray: '#808080',
-//     green: '#008000',
-//     orange: '#ffa500',
-//     purple: '#800080',
-//     red: '#ff0000',
-//     yellow: '#ffff00'
-//   };
-
-//   if (typeof colours[colour.toLowerCase()] != 'undefined')
-//     return colours[colour.toLowerCase()];
-
-//   return false;
-// }
+function colourNameToHex(colour) {
+  var colours = {
+    beige: '#f5f5dc',
+    blue: '#0000ff',
+    brown: '#a52a2a',
+    gray: '#808080',
+    green: '#008000',
+    orange: '#ffa500',
+    purple: '#800080',
+    red: '#ff0000',
+    yellow: '#ffff00'
+  };
+  return colours[colour] || '#ffffff';
+}
