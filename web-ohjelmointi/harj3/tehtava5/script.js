f4bc2cd2f77ac008a87cc1485152c6b23142de45
sudexp <sudexp@icloud.com>
2018-09-19 12:53:51 +0300

    Exercise 5 modified

--- web-ohjelmointi/harj3/tehtava5/script.js
+++ web-ohjelmointi/harj3/tehtava5/script.js
@@ -1,16 +1,21 @@
 function addTask() {
-  var ul = document.getElementById('list');
-  var li = document.createElement('li');
-  var task = document.getElementById('task');
-  var text = document.createTextNode(task.value);
+  let ul = document.getElementById('list');
+  let li = document.createElement('li');
+  li.addEventListener('click', removeLi, false);
+  let task = document.getElementById('task');
+  let text = document.createTextNode(task.value);
   ul.appendChild(li);
   li.appendChild(text);
   task.value = '';
 }
 
 function removeList() {
-  var list = document.getElementById('list');
+  let list = document.getElementById('list');
   while (list.hasChildNodes()) {
     list.removeChild(list.firstChild);
   }
+}
+
+function removeLi(event) {
+  event.target.remove();
 }
