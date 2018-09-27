a423191ae684ea61ed4dee1fd25be7900945c0f2
sudexp <sudexp@icloud.com>
2018-09-27 12:40:06 +0300

    Exercise1 modified

--- web-ohjelmointi/harj5/tehtava1/script.js
+++ web-ohjelmointi/harj5/tehtava1/script.js
@@ -1,4 +1,12 @@
-$("#myForm").submit(function (e) {
-  // remove default submit behavior
-  e.preventDefault();
+$(document).ready(function() {
+  $('#myForm').submit(function(e) {
+    // remove default submit behavior
+    e.preventDefault();
     // lue luvut lomakkeelta (ovat merkkijonoja)
+    let number1 = $('#number1').val();
+    let number2 = $('#number2').val();
+    let result = $('#result');
+    let sum = parseFloat(number1) + parseFloat(number2);
+    result.text('Sum is ' + sum);
+  });
+});
