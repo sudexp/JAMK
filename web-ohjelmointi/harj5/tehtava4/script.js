da43bbca5f770d944aa2201c1af5a877e181fd74
sudexp <sudexp@icloud.com>
2018-10-03 19:38:55 +0300

    Exercise 4 script modified

--- web-ohjelmointi/harj5/tehtava4/script.js
+++ web-ohjelmointi/harj5/tehtava4/script.js
@@ -5,27 +5,20 @@ $(document).ready(function() {
     error: function(xhr) {
       alert('An error occured: ' + xhr.status + ' ' + xhr.statusText);
     }
-  })
-    .done(function(data) {
-      console.log('done');
-      showHouses(data);
-    })
-    .fail(function() {
-      console.log('error');
-    })
-    .always(function() {
-      console.log('complete');
-    });
+  }).done(function(data) {
+    console.log('done');
+    showHouses(data);
+  });
 });
 
 function showHouses(data) {
   $.each(data.houses, function(index, value) {
-    var div = $('<div class="houseContainer"></div>');
-    var img = $('<img class="houseImage"' + 'src="' + value.image + '">');
-    var header = $('<p class="header">' + value.address + '</p>');
-    var size = $('<p>' + value.type + '</p>');
-    var text = $('<p class="description">' + value.description + '</p>');
-    var price = $('<p class="price">' + value.price + '</p>');
+    var div = $(`<div class="houseContainer"></div>`);
+    var img = $(`<img class="houseImage" src="${value.image}">`);
+    var header = $(`<p class="header">${value.address}</p>`);
+    var size = $(`<p>${value.type}</p>`);
+    var text = $(`<p class="description">${value.description}</p>`);
+    var price = $(`<p class="price">${value.price}</p>`);
     div.append(img, header, size, text, price);
     $('#houses').append(div);
   });
