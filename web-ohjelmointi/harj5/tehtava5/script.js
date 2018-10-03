5b35d13943095730a63a81713fee21bfdc332197
sudexp <sudexp@icloud.com>
2018-10-03 12:20:20 +0300

    Exercise 5 created

---
+++ web-ohjelmointi/harj5/tehtava5/script.js
@@ -0,0 +1,66 @@
+function ajax(method, url, callback) {
+  let request;
+  if (window.XMLHttpRequest) {
+    request = new XMLHttpRequest();
+  } else {
+    request = new ActiveXObject('Microsoft.XMLHTTP');
+  }
+  request.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      callback(this.responseText);
+    }
+  };
+  request.open(method, url, true);
+  request.send();
+}
+
+function showHint(str) {
+  if (str.length == 0) {
+    document.getElementById('txtHint').innerHTML = '';
+    return;
+  } else {
+    const url = 'ajax-suggest.php?q=' + str;
+    ajax('GET', url, showList);
+    // ajaxMock('GET', url, showList);
+  }
+}
+
+// function ajaxMock(method, url, callback) {
+//   setTimeout(function() {
+//     callback('Aino	Anna	Anni	Antti	Ari');
+//   }, 1000);
+// }
+
+function showList(response) {
+  let ul = document.getElementById('txtHint');
+  while (ul.hasChildNodes()) {
+    ul.removeChild(ul.firstChild);
+  }
+  const arr = response.split('	');
+  let i;
+  let len = arr.length;
+  for (i = 0; i < len; i++) {
+    let li = document.createElement('li');
+    li.addEventListener('click', showName, false);
+    let text = document.createTextNode(arr[i]);
+    ul.appendChild(li);
+    li.appendChild(text);
+  }
+}
+
+function showName(event) {
+  window.open(
+    'http://student.labranet.jamk.fi/~M0394/ttms0500/harj4/tehtava2/ajax-suggest.php?q=' +
+      this.textContent
+  );
+}
+
+document.onkeydown = function(event) {
+  event = event || window.event;
+  if (event.keyCode === 27) {
+    document.getElementById('name').value = '';
+    showHint('');
+  } else if (event.keyCode == 13) {
+    showName(event);
+  }
+};
