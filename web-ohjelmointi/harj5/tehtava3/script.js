010d7df1ccf7e16af9c22913a59c27ee285a12b9
sudexp <sudexp@icloud.com>
2018-10-03 19:28:15 +0300

    Exercise 3 script modified

--- web-ohjelmointi/harj5/tehtava3/script.js
+++ web-ohjelmointi/harj5/tehtava3/script.js
@@ -1,29 +1,7 @@
 $(document).ready(function() {
-  $('#h1').click(function() {
-    $('#p1').slideToggle('slow');
-  });
-  $('#h2').click(function() {
-    $('#p2').slideToggle('slow');
-  });
-  $('#h3').click(function() {
-    $('#p3').slideToggle('slow');
-  });
-  $('#h4').click(function() {
-    $('#p4').slideToggle('slow');
-  });
-  $('#h5').click(function() {
-    $('#p5').slideToggle('slow');
+  [1, 2, 3, 4, 5].forEach(i => {
+    $(`#h${i}`).click(function() {
+      $(`#p${i}`).slideToggle('slow');
+    });
   });
 });
-
-$(document).ready(function() {
-  $('#flip').click(function() {
-    $('#panel').slideToggle('slow');
-  });
-});
-
-// function jqToggle(number) {
-//   $('#h' + number).click(function() {
-//     $('#p' + number).slideToggle('slow');
-//   });
-// }
