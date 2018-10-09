c274ca29af2558bed4053bf658a9288b73010031
sudexp <sudexp@icloud.com>
2018-10-09 21:09:12 +0300

    drawSwedishFlag corrected, functions init and drawGuyanaFlag created

--- web-ohjelmointi/harj6/tehtava1/script.js
+++ web-ohjelmointi/harj6/tehtava1/script.js
@@ -1,3 +1,8 @@
+function init() {
+  drawSwedenFlag();
+  drawGuyanaFlag();
+}
+
 function drawSwedenFlag() {
   const canvas = document.getElementById('sweden');
   const ctx = canvas.getContext('2d');
@@ -14,7 +19,21 @@ function drawSwedenFlag() {
 
   ctx.fillStyle = colorYellow;
   ctx.fillRect(canvas.width / 3.5, 0, canvas.width / 10, canvas.height);
-  ctx.closePath();
 }
 
-window.onload = drawSwedenFlag;
+function drawGuyanaFlag() {
+  const canvas = document.getElementById('guyana');
+  const ctx = canvas.getContext('2d');
+  canvas.height = 500;
+  canvas.width = 1000;
+  const colorGreen = '#008000';
+  const colorWhite = '#ffffff';
+  const colorGoldenRod = '#DAA520';
+  const colorBlack = '#000000';
+  const colorFireBrick = '#b22222';
+
+  ctx.fillStyle = colorGreen;
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
+}
+
+window.onload = init;
