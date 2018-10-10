d0f92daf16fa6c0d35e705201ab15ee70bb82a44
sudexp <sudexp@icloud.com>
2018-10-10 20:49:01 +0300

    exercise4 js modified

--- web-ohjelmointi/harj6/tehtava4/script.js
+++ web-ohjelmointi/harj6/tehtava4/script.js
@@ -1,20 +1,25 @@
-function drawToCanvas(d) {
-  let canvas = document.getElementById('myCanvas');
-  let ctx = canvas.getContext('2d');
+let initialized = false;
+let canvas, ctx;
+const r = 100;
+const l = 100;
+const color = '#00FFFF';
+
+function initCanvas() {
+  canvas = document.getElementById('myCanvas');
+  ctx = canvas.getContext('2d');
   canvas.height = 500;
   canvas.width = 1000;
   ctx.clearRect(0, 0, canvas.height, canvas.width);
+}
 
-  const r = 100;
+function drawCircle() {
   ctx.beginPath();
   ctx.arc(canvas.width / 2, canvas.height / 2, 2 * r, 0 * Math.PI, 2 * Math.PI);
   ctx.stroke();
   ctx.closePath();
+}
 
-  const h = r - d;
-  const vari = '#00FFFF';
-  const initialAngle = Math.PI / 2 - Math.acos(h / r);
-
+function drawWater(initialAngle) {
   ctx.beginPath();
   ctx.arc(
     canvas.width / 2,
@@ -23,21 +28,20 @@ function drawToCanvas(d) {
     initialAngle,
     Math.PI - initialAngle
   );
-  ctx.fillStyle = vari;
+  ctx.fillStyle = color;
   ctx.fill();
   ctx.stroke();
   ctx.closePath();
+}
 
-  const l = 100;
+function showStat(initialAngle) {
+  // Calculate volume of cylinder:
   let v = (Math.PI * r * r * l) / 1000;
   v = v.toFixed(2);
 
+  // Draw stats
   let s = 0;
   s = (((r * r) / 2) * (initialAngle - Math.sin(initialAngle)) * l) / 1000;
-
-  console.log('r= ', r);
-  console.log('initialAngle= ', initialAngle);
-  console.log('Math.sin(initialAngle) ', Math.sin(initialAngle));
   s = s.toFixed(2);
 
   ctx.fillStyle = '#000000';
@@ -48,4 +52,19 @@ function drawToCanvas(d) {
   ctx.fillText(`Nestemäärä: ${s} litraa`, 20, 100);
 }
 
-window.onload = drawToCanvas;
+function init() {
+  initCanvas();
+  drawCircle();
+}
+
+function drawToCanvas(d) {
+  initCanvas();
+  // Calculate params:
+  const h = r - d;
+  const initialAngle = Math.PI / 2 - Math.acos(h / r);
+  drawWater(initialAngle);
+  drawCircle();
+  showStat(initialAngle);
+}
+
+window.onload = init;
