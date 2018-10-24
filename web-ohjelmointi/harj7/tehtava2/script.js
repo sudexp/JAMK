4f35f897d5e08999430403146232e370f0f4c108
sudexp <sudexp@icloud.com>
2018-10-24 10:31:31 +0300

    exercise 2 modified

--- web-ohjelmointi/harj7/tehtava2/script.js
+++ web-ohjelmointi/harj7/tehtava2/script.js
@@ -3,13 +3,14 @@ function drawChart() {
   Chart.defaults.global.defaultFontFamily = 'Lato';
   Chart.defaults.global.defaultFontSize = 18;
   Chart.defaults.global.defaultFontColor = 'grey';
+  Chart.defaults.global.animation.duration = '5000';
   var chart = new Chart(ctx, {
     type: 'bar',
     data: {
       labels: ['Ford', 'Opel', 'Toyota'],
       datasets: [
         {
-          label: 'Autojen lukumäärät',
+          label: 'Lukumäärä (kpl)',
           data: [7, 3, 8],
           backgroundColor: [
             'rgba(59, 142, 211, 1.0)',
@@ -21,26 +22,21 @@ function drawChart() {
     },
     options: {
       responsive: false,
-      legend: {
+      title: {
         display: true,
-        position: 'top',
-        labels: {
-          boxWidth: 50,
-          fontColor: 'rgba(rgba(30, 30, 30, 1.0)'
-        }
+        text: 'Autojen lukumäärät',
+        fontSize: '20',
+        fontColor: 'rgba(30, 30, 30, 1.0)'
+      },
+      legend: {
+        display: false
       },
       scales: {
-        yAxes: [{ barThickness: 100, ticks: { beginAtZero: true } }],
-        xAxes: [{ barThickness: 100 }]
+        yAxes: [{ ticks: { beginAtZero: true } }],
+        xAxes: [{ barThickness: 120 }]
       }
     }
   });
-  // borderColor: [
-  //   'rgba(59, 142, 211, 1.0)',
-  //   'rgba(137, 97, 150, 1.0)',
-  //   'rgba(79, 181, 158, 1.0)'
-  // ],
-  // borderWidth: 1 // The data for our dataset // Configuration options go here
 }
 
 window.onload = drawChart;
