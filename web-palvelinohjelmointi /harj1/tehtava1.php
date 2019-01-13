dfe71ef4a90f459a9eb6d0ba55f44328be5a42c5
sudexp <sudexp@icloud.com>
2019-01-13 23:14:15 +0200

    web-palvelinohjelmonti folder created, harj1 folder created, tehtava1.php created

---
+++ web-palvelinohjelmointi /harj1/tehtava1.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <meta charset="utf-8">
+  <title>Dmitry Sklyarov [M0394] Harjoitukset 1 / Tehtävä 1</title>
+  <meta name="keywords" lang="fi" content="Dmitry Sklyarov, Jyväskylän ammattikorkeakoulu"/>
+  <meta name="description" lang="fi" content="Dmitry Sklyarovin tehtava 1"/>
+  <meta name="author" lang="fi" content="Dmitry Sklyarov"/>
+  <meta name="viewport" content="width=device-width, initial-scale=1"/>
+</head>
+
+<body>
+</body>
+
+</html>
