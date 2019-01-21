41215f8f9087f4744a25c7d2dd4882384c995c35
sudexp <sudexp@icloud.com>
2019-01-21 10:02:47 +0200

    harj2/t3_millionaire.php modified

--- web-palvelinohjelmointi /harj2/t3_millionaire.php
+++ web-palvelinohjelmointi /harj2/t3_millionaire.php
@@ -15,11 +15,20 @@
 
 if (isset($_GET['nappi'])) {
   $summa = 0;
-  $summa = $summa + $_GET['kysymys1'];
+
+  if (isset($_GET['kysymys1'])) {
+    $summa += $_GET['kysymys1'];
+  }
+
 
   if (isset($_GET['kysymys2'])) {
-      // if(is_array($_GET['kysymys2'])) {
     foreach ($_GET['kysymys2'] as $pojot) {
+      $summa += $pojot;
+    }
+  }
+
+  if (isset($_GET['kysymys3'])) {
+    foreach ($_GET['kysymys3'] as $pojot) {
       $summa += $pojot;
     }
   }
@@ -29,14 +38,24 @@ if (isset($_GET['nappi'])) {
 
 $lomake = <<<EOLomake
   <form method="get" action="{$_SERVER['PHP_SELF']}">
+    Kysymys 1: Mikä vaihtoehto on oikea?<br>
     <input type='radio' name='kysymys1' value='1'>Hauki on kala<br>\n
     <input type='radio' name='kysymys1' value='0'>Hauki on linttu<br>\n
     <input type='radio' name='kysymys1' value='0'>Hauki on kissa<br>\n
-    <br><br>
-    <input type='checkbox' name='kysymys2[]' value='3'>Mega on miljoona<br>\n
-    <input type='checkbox' name='kysymys2[]' value='-3'>Giga on miljoona<br>\n
-    <input type='checkbox' name='kysymys2[]' value='-3'>Tera on miljoona<br>\n
-    <input type="submit" name="nappi" value="Send">
+    <br>
+    Kysymys 2: Mitä kirjainlyhenne PHP tarkoittaa puhuttaessa Web-ohjelmoinnista?<br>
+    <select name="kysymys2[]">
+      <option value='0'>Valitse</option>
+      <option value='-2'>Peaceful Human Programing</option>
+      <option value='2'>PHP: Hypertext Preprocessor</option>
+      <option value='-2'>Professional hypertext parser</option>
+    </select><br><br>
+    Kysymys 3: Mikä vaihtoehto on oikea?<br>
+    <input type='checkbox' name='kysymys3[]' value='3'>Mega on miljoona<br>\n
+    <input type='checkbox' name='kysymys3[]' value='-3'>Giga on miljoona<br>\n
+    <input type='checkbox' name='kysymys3[]' value='-3'>Tera on miljoona<br>\n
+    <br>  
+    <input type="submit" name="nappi" value="Nayta pisteet">
   </form>
 EOLomake;
 
