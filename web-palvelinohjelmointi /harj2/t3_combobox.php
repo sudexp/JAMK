6039d4fe712e9603e53508d6e2eb986f3ee6e1da
sudexp <sudexp@icloud.com>
2019-01-21 10:12:21 +0200

    harj2/t3_combobox.php created

---
+++ web-palvelinohjelmointi /harj2/t3_combobox.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <meta charset="utf-8">
+  <title>Dmitry Sklyarov [M0394] Harjoitukset 2 / Tehtävä 3</title>
+  <meta name="keywords" lang="fi" content="Dmitry Sklyarov, Jyväskylän ammattikorkeakoulu"/>
+  <meta name="description" lang="fi" content="Dmitry Sklyarovin tehtava 3"/>
+  <meta name="author" lang="fi" content="Dmitry Sklyarov"/>
+  <meta name="viewport" content="width=device-width, initial-scale=1"/>
+</head>
+
+<body>
+
+</body>
+
+</html>
