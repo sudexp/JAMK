a238e6aea687ec1fce276fc51404d8d5dae39b85
sudexp <sudexp@icloud.com>
2019-01-21 14:50:42 +0200

    harj3/t3_one_hahded_bandit.php modified

--- web-palvelinohjelmointi /harj3/t3_one_hahded_bandit.php
+++ web-palvelinohjelmointi /harj3/t3_one_hahded_bandit.php
@@ -11,7 +11,37 @@
 </head>
 
 <body>
+  <?php
+  session_start();
 
+  $loggedIn = false;
+
+  $users = array(
+    array("id" => "admin", "passwd" => "qwerty12"),
+    array("id" => "user", "passwd" => "asdfgh"),
+    array("id" => "Ari", "passwd" => "root66")
+  );
+
+  if (isset($_POST['id']) && isset($_POST['passwd'])) {
+    foreach ($users as $user) {
+      if ($_POST['id'] === $user['id'] && $_POST['passwd'] === $user['passwd']) {
+        $loggedIn = true;
+        $currentUser = $user['id'];
+      }
+    }
+    if ($loggedIn === true) {
+      echo "You are logged in as " . $currentUser;
+    } else {
+      echo "Wrong username or password!";
+    }
+  }
+  ?>
+  
+  <form method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>">
+    Username:<br><input type="text" name="id"><br>
+    Password:<br><input type="text" name="passwd"><br>
+    <input type='submit' name='action' value='Log In'>
+  </form>
 </body>
 
 </html>
