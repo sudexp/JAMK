608b13059cf9989d4fbd454254e2236f7be77ece
sudexp <sudexp@icloud.com>
2019-01-21 10:49:03 +0200

    harj2/t3_combobox.php modified

--- web-palvelinohjelmointi /harj2/t3_combobox.php
+++ web-palvelinohjelmointi /harj2/t3_combobox.php
@@ -11,7 +11,38 @@
 </head>
 
 <body>
+  <?php
 
+  $links[0][0] = "<a href='https://www.mtvuutiset.fi/aihe/urheilu/2963748' target='_blank'>MTV Sport</a>";
+  $links[0][1] = "<a href='https://yle.fi/urheilu' target='_blank'>Yle Urheilu</a>";
+  $links[1][0] = "<a href='https://musicfinland.fi/' target='_blank'>MusicFinland</a>";
+  $links[1][1] = "<a href='http://suomenmusiikki.fi/' target='_blank'>Suomen musikki</a>";
+  $links[2][0] = "<a href='https://evl.fi/etusivu' target='_blank'>Suomen evankelis-luterilainen kirkko</a>";
+  $links[2][1] = "<a href='https://ort.fi' target='_blank'>Suomen ortodoksinen kirkko</a>";
+
+  $form = <<<EOForm
+    <form method='get' action={$_SERVER['PHP_SELF']}>
+      <select name='topics[]' size='3' multiple>
+        <option value='0'>Urheilu</option>
+        <option value='1'>Musiikki</option>
+        <option value='2'>Kirkko</option>
+      </select><br><br>
+      <input type='submit' name='button' value='Show links'><br>
+    </form>
+EOForm;
+
+  echo '<br>' . $form . '<br>';
+
+  if (isset($_GET['button'])) {
+    foreach ($_GET['topics'] as $topic) {
+      foreach ($links[$topic] as $link) {
+        echo $link . '<br>';
+      }
+    }
+    echo '<br>';
+  }
+
+  ?>
 </body>
 
 </html>
