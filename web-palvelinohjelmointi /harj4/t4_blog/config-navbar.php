33f7d2f47a3ee0585c03ca76375cc7e7734d4c9e
sudexp <sudexp@icloud.com>
2019-01-23 14:22:17 +0200

    harj4/t4_blog/config-navbar.php created

---
+++ web-palvelinohjelmointi /harj4/t4_blog/config-navbar.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <meta charset="utf-8">
+  <title>Tallensit blogimerkinnän</title>
+  <meta name="keywords" lang="fi" content="Dmitry Sklyarov, Jyväskylän ammattikorkeakoulu"/>
+  <meta name="description" lang="fi" content="Dmitry Sklyarovin tehtava 4"/>
+  <meta name="author" lang="fi" content="Dmitry Sklyarov"/>
+  <meta name="viewport" content="width=device-width, initial-scale=1"/>
+  <link rel="stylesheet" type="text/css" href="tyyli.css">
+</head>
+
+<body>
+  <?php
+  $datadir = "data2";
+  ?>
+
+  [<a href="blogi-lista.php">Lue blogia</a>]
+  [<a href="blogi-lomake.php">Lisää merkintä</a>]
+</body>
+
+</html>
