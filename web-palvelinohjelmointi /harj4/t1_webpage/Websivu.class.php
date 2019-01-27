a62bc88075efd40c3fe229284d6cd5dc6339b0af
sudexp <sudexp@icloud.com>
2019-01-27 10:40:37 +0200

    harj4/t1_webpage/Websivu.class.php created

---
+++ web-palvelinohjelmointi /harj4/t1_webpage/Websivu.class.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <meta charset="utf-8">
+  <title>Harjoitus 4, Tehtava 1 - Websivu</title>
+  <meta name="keywords" lang="fi" content="Dmitry Sklyarov, Jyväskylän ammattikorkeakoulu"/>
+  <meta name="description" lang="fi" content="Dmitry Sklyarovin tehtava 1"/>
+  <meta name="author" lang="fi" content="Dmitry Sklyarov"/>
+  <meta name="viewport" content="width=device-width, initial-scale=1"/>
+</head>
+
+<body>
+  
+</body>
+
+</html>
