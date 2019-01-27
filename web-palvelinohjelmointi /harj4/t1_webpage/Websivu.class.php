66f8c2196d795902e3b8df75fd6b47c6f8448d75
sudexp <sudexp@icloud.com>
2019-01-27 10:42:51 +0200

    harj4/t1_webpage/Websivu.class.php modified

--- web-palvelinohjelmointi /harj4/t1_webpage/Websivu.class.php
+++ web-palvelinohjelmointi /harj4/t1_webpage/Websivu.class.php
@@ -11,7 +11,55 @@
 </head>
 
 <body>
-  
+  <?php
+
+  class Websivu
+  {
+    protected $otsikko;
+    protected $avainsanat;
+    protected $sisalto;
+
+    function __construct($otsikko, $avainsanat)
+    {
+      $this->otsikko = $otsikko;
+      $this->avainsanat = $avainsanat;
+    }
+
+    function __destuct()
+    {
+      $this->otsikko = null;
+      $this->avainsanat = null;
+      $this->sisalto = null;
+    }
+
+    public function asetaBodyelementinSisalto($data)
+    {
+      $this->sisalto = $data;
+    }
+
+    public function naytaSivu()
+    {
+      echo "<html><head>\n";
+      $this->tulostaOtsikkoelementti();
+      echo "</head></body>\n";
+      echo $this->sisalto;
+      echo "</body></html>\n";
+
+    }
+
+    private function tulostaOtsikkoelementti()
+    {
+      echo "<title>$this->otsikko</title>";
+    }
+
+    private function tulostaAvainsanaelementti()
+    {
+        //return $this->name;
+      echo "<meta name=keywords>$this->avainsanat</meta>\n";
+    }
+  }
+
+  ?> 
 </body>
 
 </html>
