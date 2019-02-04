981ff498a43b2fae6249b3d71269198905a45162
sudexp <sudexp@icloud.com>
2019-02-04 12:51:35 +0200

    harj5/navbar.php added

---
+++ web-palvelinohjelmointi /harj5/navbar.php
@@ -0,0 +1,9 @@
+<form method='get' action='abook-lista.php'>
+  [ <a href='abook-lista.php'>Näytä kakki osoitteet</a> ]
+  [ <a href='abook-lisayslomake.php'>Lisää osoite</a> ]
+  <!-- [ <a href='abook-lisaa.php'>Lisätä data</a> ] -->
+  Etsi:
+  <input type='text' size='10' name='hakuehto' value=''>
+</form>
+<hr>
+<h2 style="background-color: #ffeedd">Osoitekirja</h2>
